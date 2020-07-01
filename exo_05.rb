fd86468ae0a4613c8d7e1f23f18d29a99facfc19
emilie-lb <62951146+emilie-lb@users.noreply.github.com>
2020-07-01 15:58:25 +0200

    Update exo_05.rb

--- exo_05.rb
+++ exo_05.rb
@@ -1,28 +1,28 @@
 # affiche la chaine de caractère contenue dans les ""
 puts "On va compter le nombre d'heures de travail à THP"
 
-# affiche "Travail: et exécute le contenu de #{} (ici ça fait le calcul)
+# affiche "Travail:" et exécute puis affiche le contenu de #{} (ici = fait le calcul)
 puts "Travail: #{10*5*11}"
 
-# affiche "En minutes ça fait:" et exécute le contenu de #{} (ici ça fait le calcul)
+# affiche "En minutes ça fait:" et exécute puis affiche le contenu de #{} (ici = fait le calcul)
 puts "En minutes ça fait: #{15*5*11*60}"
 
 # affiche la chaine de caractère contenue dans les ""
 puts "Et en secondes?"
 
-# fait le calcul
+# fait le calcul et affiche résultat
 puts 10*5*11*60*60
 
 # affiche la chaine de caractère contenue dans les ""
 puts "Est-ce que c'est vrai que 3+2 < 5-7"
 
-# fait le test de condition et renvoie false dans la console
+# fait la comparaison et renvoie false dans la console
 puts 3+2 < 5-7
 
-# affiche "Ca fait combien 3+2 ?" et exécute le contenu de #{} (ici ça fait le calcul)
+# affiche "Ca fait combien 3+2 ?" et exécute puis affiche le contenu de #{} (ici = fait le calcul)
 puts "Ca fait combien 3+2 ? #{3+2}"
 
-# affiche "Ca fait combien 5-7 ?" et exécute le contenu de #{} (ici ça fait le calcul)
+# affiche "Ca fait combien 5-7 ?" et exécute puis affiche le contenu de #{} (ici = fait le calcul)
 puts "Ca fait combien 5-7 ? #{5-7}"
 
 # affiche la chaine de caractère contenue dans les ""
@@ -31,7 +31,7 @@ puts "Ok, c'est faux alors?"
 # affiche la chaine de caractère contenue dans les ""
 puts "C'est drôle ça, faison-en plus:"
 
-# ces trois lignes: affichent la question et exécute le contenu de #{} (ici tests de condition et retourne true ou false)
+# ces trois lignes: affichent la question et exécutent puis affichent le contenu de #{} (ici fait comparaison et retourne true ou false)
 puts "Est ce que 5 est plus grand que -2? #{5 > -2}"
 puts "Est ce que 5 est supérieur ou égal à -2? #{5 >= -2}"
 puts "Est ce que 5 est inférieur ou égal à -2? #{5 <= -2}"
