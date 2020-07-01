45265acf26d6da69876807466a1e90ac26b14793
emilie-lb <62951146+emilie-lb@users.noreply.github.com>
2020-07-01 16:20:47 +0200

    Update exo_10.rb

--- exo_10.rb
+++ exo_10.rb
@@ -1,5 +1,5 @@
 puts "En quelle année es-tu né(e)?"
 print "> "
-# avec to_i, la chaine de caractère (string) est converti en nombre entier (integer)
+# avec to_i, la chaine de caractère (string) est convertie en nombre entier (integer)
 user_yearbirth = gets.chomp.to_i
 puts "En 2017, tu avais #{2017 - user_yearbirth} ans."
